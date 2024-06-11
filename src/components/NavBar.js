6b4ea60b3515430c1462bdfffa9235ddcd4cdbca
RutulPatel007 <116881681+RutulPatel007@users.noreply.github.com>
2024-06-11 23:19:15 +0530

    navbar adjust

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container,NavDropdown } from "react-bootstrap";
 import logo from '../assets/img/CHEMZONES.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/github-mark-c791e9551fe4/github-mark/github-mark-white.png';
+import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -59,8 +59,7 @@ export const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link as={Link} to="/aboutus" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>About Us</Nav.Link>
-              <Nav.Link as={Link} to="/quality" className={activeLink === 'quality' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('quality')}>Quality</Nav.Link>
+              
               
               <NavDropdown     
        title={
@@ -81,12 +80,16 @@ export const NavBar = () => {
         <NavDropdown.Item  onClick={()=>redirectToProjects("product4")}>Dyes Intermediate</NavDropdown.Item>
         {/* Add more dropdown items as needed */}
       </NavDropdown>
+      
+              <Nav.Link as={Link} to="/quality" className={activeLink === 'quality' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('quality')}>Quality</Nav.Link>
+              <Nav.Link as={Link} to="/aboutus" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>About Us</Nav.Link>
             </Nav>
+
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/rutul-patel-17378024a/"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/RutulPatel007"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/rutul_patel_007/"><img src={navIcon3} alt="" /></a>
+                <a href=""><img src={navIcon1} alt="" /></a>
+                <a href=""><img src={navIcon2} alt="" /></a>
+                <a href=""><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='/connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
